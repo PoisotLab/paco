a77a2036e9100ffdd442e3476d3b7a7574bd6549
matthutchinson1 <mch183@uclive.ac.nz>
2015-01-30 14:49:45 +1300

    full complement of methods

--- R/PACo.r
+++ R/PACo.r
@@ -2,7 +2,7 @@
 #' @param D a list with the data
 #' @param nperm Number of permutations
 #' @param seed Seed if results need to be reproduced
-#' @param method The method to permute matrices with: "r0", "r1", "r2", "c0", "swap", "quasiswap"
+#' @param method The method to permute matrices with: "r0", "r1", "r2", "c0", "swap", "quasiswap", "backtrack", "tswap", "r00"
 #' @export
 #' @examples 
 #' data(gopherlice)
@@ -11,10 +11,11 @@
 #' ldist <- cophenetic(licetree)
 #' D <- prepare_paco_data(gdist, ldist, gl_links)
 #' D <- add_pcoord(D)
-#' D <- PACo(D, nperm=10, seed=42)
+#' D <- PACo(D, nperm=10, seed=42, method="r0")
 #' print(D$gof)
-PACo <- function(D, nperm=1000, seed=NA, method="NA")
+PACo <- function(D, nperm=1000, seed=NA, method="r0")
 {
+   method <- match.arg(method, c("r0", "r1", "r2", "r00", "c0", "swap", "tswap", "backtrack", "quasiswap"))
    if(!("H_PCo" %in% names(D))) D <- add_pcoord(D)
    proc <- vegan::procrustes(X=D$H_PCo, Y=D$P_PCo)
    Nlinks <- sum(D$HP)
@@ -24,7 +25,7 @@ PACo <- function(D, nperm=1000, seed=NA, method="NA")
    if(!is.na(seed)) set.seed(seed)
    for(n in c(1:nperm))
    {
-      permuted_HP <- commsimulator(D$HP, method)
+      permuted_HP <- vegan::commsimulator(D$HP, method)
       permuted_HP <- permuted_HP[rownames(D$HP),colnames(D$HP)]
       perm_D <- list(H=D$H, P=D$P, HP=permuted_HP)
       perm_paco <- add_pcoord(perm_D)
