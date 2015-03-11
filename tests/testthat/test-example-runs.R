0cc541a2eb53af7a2b16b41d855f985ad5814d75
efcaguab <efcaguab@gmail.com>
2015-03-11 16:26:22 +1300

    added paco-links to the test

--- tests/testthat/test-example-runs.R
+++ tests/testthat/test-example-runs.R
@@ -4,7 +4,7 @@ run_example <- function (H, P, HP) {
   D <- prepare_paco_data(H, P, HP)
   D <- add_pcoord(D)
   D <- PACo(D, nperm = 10, seed = 42, method="r0")
-  return (D$gof)
+  D <- paco_links (D, nperm = 10, seed = 42)
 }
 
 test_that ("paco example works as expected", {
@@ -14,7 +14,7 @@ test_that ("paco example works as expected", {
   gdist <- cophenetic(gophertree)
   ldist <- cophenetic(licetree)
   
-  expect_equal (run_example (gdist, ldist, gl_links)$p, 0)
-  expect_equal (run_example (gdist, ldist, gl_links)$n, 10)
+  expect_is (run_example (gdist, ldist, gl_links)$gof$p, "numeric")
+  expect_is (run_example (gdist, ldist, gl_links)$gof$n, "numeric")
   
 })
