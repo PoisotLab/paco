f56ff7c8aad5a4637737d2149e7e81147fa8e9ce
efcaguab <efcaguab@gmail.com>
2015-03-13 15:18:29 +1300

    actually doing it changed the wrong line in last commit

--- R/paco_links.r
+++ R/paco_links.r
@@ -52,8 +52,8 @@ paco_links <- function(D, .parallel = FALSE, .progress = "none", ...)
 single_paco_link <- function (D, HP.ones, i,...) {
   HP_ind <- D$HP
   HP_ind[HP.ones[i,1],HP.ones[i,2]]=0
-  PACo.ind <- PACo(list(H=D$H, P=D$P, HP=HP_ind), method=D$method, ...)
-  Proc.ind <- add_pcoord(list(H=D$H, P=D$P, HP=HP_ind)) 
+  PACo.ind <- add_pcoord(list(H=D$H, P=D$P, HP=HP_ind))
+  Proc.ind <- vegan::procrustes(X=PACo.ind$H_PCo, Y=PACo.ind$P_PCo) 
   res.Proc.ind <- c(residuals(Proc.ind))
   res.Proc.ind <- append(res.Proc.ind, NA, after= i-1)
 }
