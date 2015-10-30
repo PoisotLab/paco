028637202e83ca53ee7868383d21a3f0ad37e063
Fernando Cagua <efc29@Fernandos-MacBook-Pro.local>
2015-10-30 12:28:37 +0100

    fix type argument problem

--- R/residuals.paco.r
+++ R/residuals.paco.r
@@ -1,16 +1,13 @@
 #' Get procrustes residuals from a paco object
 #' @param object a list with the data
-#' @param ... Used for type, wether the whole residual matrix (\code{matrix}) or the residuals per interaction (\code{interaction}) is desired
+#' @param type wether the whole residual matrix (\code{matrix}) or the residuals per interaction (\code{interaction}) is desired
 #' @export
 
-residuals.paco <- function (object, ...) {
-  if(missing(type)) stop("Please specify type")
+residuals.paco <- function (object, type = "interaction") {
   type <- match.arg(type, c("matrix", "interaction"))
 
-  if (!exists("proc", object)) stop ("Procrustes object 'proc' not found")
-
-  distance <- object$proc$X - object$proc$Yrot
-  rownames(distance) <- paste(rownames(object$proc$X), rownames(object$proc$Yrot), sep="-") #colnames identify the H-P link
+  distance <- object$X - object$Yrot
+  rownames(distance) <- paste(rownames(object$X), rownames(object$Yrot), sep="-") #colnames identify the H-P link
 
   if (type == "matrix") {
     return (distance)
