3e1e54f8c10b7a77c042ec6ea39b992b99db5a3b
matthutchinson1 <matthewhutchinson15@gmail.com>
2016-09-26 18:17:17 -0400

    doc

--- R/PACo.r
+++ R/PACo.r
@@ -1,9 +1,12 @@
-#' Performs PACo/procustes analysis
-#' @param D a list with the data
-#' @param nperm Number of permutations
-#' @param seed Seed if results need to be reproduced
-#' @param method The method to permute matrices with: "r0", "r1", "r2", "c0", "swap", "quasiswap", "backtrack", "tswap", "r00". See \code{\link[vegan]{commsim}} for details
-#' @param symmetric Use symmetric Procrustes statistic 
+#' Performs PACo analysis. Two sets of Principle Coordinates are superimposed by Procrustes superimposition. The sum of squared residuals of this superimposition give an indication of how congruent the two datasets are. For example, in a biological system the two sets of Principle Coordinates can be composed from the phylogenetic distance matrices of two interacting groups. The congruence measured by PACo indicates how concordant the two phylogenies are based on observed ecological interactions between them.
+#' @param D A list of class `paco' as returned by paco::add_pcoord which includes Principle Coordinates for both phylogenetic distance matrices.
+#' @param nperm The number of permutations to run. Whereby in each permutation, the network is randomized following the method argument and phylogenetic congruence between phylogenies is reassessed.
+#' @param seed An integer with which to begin the randomizations. If the same seed is used the randomizations will be the same and results reproducible. If 'NA' a random seed is chosen.
+#' @param method Character string. The method to permute matrices with: "r0", "r1", "r2", "c0", "swap", "quasiswap", "backtrack", "tswap", "r00". Briefly, "r00" produces the least conservative null model as it only maintains total fill (i.e., total number of interactions). "r0" and "c0" maintain the row sums and column sums, respectively, as well as the total number of interactions. "backtracking" and any of the "swap" algorithms conserve the total number of interactions in the matrix, as well as both row and column sums. Finally, "r1" and "r2" conserve the row sums, the total number of interactions, and randomized based on observed interaction frequency. See vegan::commsim for more details.
+#' @param symmetric Logical. Whether or not to use the symmetric Procrustes statistic or not. When TRUE, the symmetric statistic is used. When FALSE, the asymmetric is used. A decision on which to use is based on whether one group is assumed to track the evolution of the other, or not.
+#' @param proc.warnings Logical. Make any warnings from the Procrustes superimposition callable. If TRUE, any warnings are viewable with the warnings() command. If FALSE, warnings are internally suppressed. Default is TRUE 
+#' @param shuffled Logical. Return the Procrustes sum of squared residuals for every permutation of the network. When TRUE, the Procrustes statistic of all permutations is returned as a vector. When FALSE, they are not returned.
+#' @value
 #' @export
 #' @examples
 #' data(gopherlice)
@@ -14,7 +17,7 @@
 #' D <- add_pcoord(D)
 #' D <- PACo(D, nperm=10, seed=42, method="r0", correction='cailliez')
 #' print(D$gof)
-PACo <- function(D, nperm=1000, seed=NA, method="r0", symmetric = FALSE, proc.warnings=TRUE)
+PACo <- function(D, nperm=1000, seed=NA, method="r0", symmetric = FALSE, proc.warnings=TRUE, shuffled=FALSE)
 {
    correction <- D$correction
    method <- match.arg(method, c("r0", "r1", "r2", "r00", "c0", "swap", "tswap", "backtrack", "quasiswap"))
@@ -32,18 +35,36 @@ PACo <- function(D, nperm=1000, seed=NA, method="r0", symmetric = FALSE, proc.wa
    # Create randomised matrices
    null_model <- vegan::nullmodel (D$HP, method)
    randomised_matrices <- stats::simulate (null_model, nsim = nperm)
-   for(n in c(1:nperm))
-   {
-      permuted_HP <- randomised_matrices[, , n]
-      permuted_HP <- permuted_HP[rownames(D$HP),colnames(D$HP)]
-      perm_D <- list(H=D$H, P=D$P, HP=permuted_HP)
-      perm_paco <- add_pcoord(perm_D, correction=correction)
-      if(proc.warnings==TRUE){
-         perm_proc_ss <- vegan::procrustes(X=perm_paco$H_PCo, Y=perm_paco$P_PCo, symmetric= symmetric)$ss
-      }else{
-         perm_proc_ss <- suppressWarnings(vegan::procrustes(X=perm_paco$H_PCo, Y=perm_paco$P_PCo, symmetric= symmetric)$ss)
+   if(shuffled==TRUE){
+      rands <- c()
+      for(n in c(1:nperm))
+      {
+         permuted_HP <- randomised_matrices[, , n]
+         permuted_HP <- permuted_HP[rownames(D$HP),colnames(D$HP)]
+         perm_D <- list(H=D$H, P=D$P, HP=permuted_HP)
+         perm_paco <- add_pcoord(perm_D, correction=correction)
+         if(proc.warnings==TRUE){
+            perm_proc_ss <- vegan::procrustes(X=perm_paco$H_PCo, Y=perm_paco$P_PCo, symmetric= symmetric)$ss
+         }else{
+            perm_proc_ss <- suppressWarnings(vegan::procrustes(X=perm_paco$H_PCo, Y=perm_paco$P_PCo, symmetric= symmetric)$ss)
+         }
+         if(perm_proc_ss <= m2ss) pvalue <- pvalue + 1
+         rands <- c(rands, perm_proc_ss) 
       }
-      if(perm_proc_ss <= m2ss) pvalue <- pvalue + 1
+   }else{
+      for(n in c(1:nperm))
+      {
+         permuted_HP <- randomised_matrices[, , n]
+         permuted_HP <- permuted_HP[rownames(D$HP),colnames(D$HP)]
+         perm_D <- list(H=D$H, P=D$P, HP=permuted_HP)
+         perm_paco <- add_pcoord(perm_D, correction=correction)
+         if(proc.warnings==TRUE){
+            perm_proc_ss <- vegan::procrustes(X=perm_paco$H_PCo, Y=perm_paco$P_PCo, symmetric= symmetric)$ss
+         }else{
+            perm_proc_ss <- suppressWarnings(vegan::procrustes(X=perm_paco$H_PCo, Y=perm_paco$P_PCo, symmetric= symmetric)$ss)
+         }
+         if(perm_proc_ss <= m2ss) pvalue <- pvalue + 1
+      }
    }
    pvalue <- pvalue / nperm
    D$proc <- proc
@@ -51,5 +72,6 @@ PACo <- function(D, nperm=1000, seed=NA, method="r0", symmetric = FALSE, proc.wa
    D$method <- method
    D$symmetric <- symmetric
    D$correction <- correction
+   if(exists('rands')==TRUE) D$shuffled <- rands
    return(D)
 }
