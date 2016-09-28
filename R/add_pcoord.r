89ac2b353a852b5c7773abbe1f51cf5fef958fda
matthutchinson1 <matthewhutchinson15@gmail.com>
2016-09-28 10:00:10 -0400

    spelling error in documentation

--- R/add_pcoord.r
+++ R/add_pcoord.r
@@ -1,10 +1,10 @@
-#' Principle Coordinates analysis of phylogenetic distance matrices
+#' Principal Coordinates analysis of phylogenetic distance matrices
 #'
-#' Translates the distance matrices of 'host' and 'parasite' phylogenies into Principle Coordinates, as needed for Procrustes superimposition.
+#' Translates the distance matrices of 'host' and 'parasite' phylogenies into Principal Coordinates, as needed for Procrustes superimposition.
 #' @param D A list with objects H, P, and HP, as returned by \code{paco::prepare_paco_data}.
-#' @param correction In some cases, phylogenetic distance matrices are non-Euclidean which generates negative eigenvalues when those matrices are translated into Principle Coordinates. There are several methods to correct negative eigenvalues. Correction options available here are "cailliez", "lingoes", and "none". The "cailliez" and "lingoes" corrections add a constant to the eigenvalues to make them non-negative. Default is "none". 
-#' @return The list that was input as the argument `D' with two new elements; the Principle Coordinates of the `host' distance matrix and the Principle Coordinates of the `parasite' distance matrix.
-#' @note To find the Principle Coordinates of each distance matrix, we internally use a function, \code{coordpcoa}, that is a modified version of \code{ape::pcoa}, using \code{vegan::eigenvals}.
+#' @param correction In some cases, phylogenetic distance matrices are non-Euclidean which generates negative eigenvalues when those matrices are translated into Principal Coordinates. There are several methods to correct negative eigenvalues. Correction options available here are "cailliez", "lingoes", and "none". The "cailliez" and "lingoes" corrections add a constant to the eigenvalues to make them non-negative. Default is "none". 
+#' @return The list that was input as the argument `D' with two new elements; the Principal Coordinates of the `host' distance matrix and the Principal Coordinates of the `parasite' distance matrix.
+#' @note To find the Principal Coordinates of each distance matrix, we internally use a function, \code{coordpcoa}, that is a modified version of \code{ape::pcoa}, using \code{vegan::eigenvals}.
 #' @export
 #' @examples
 #' data(gopherlice)
